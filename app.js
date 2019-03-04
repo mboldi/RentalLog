ecffbaae0e09efd0d11808a041ecb9862c87d53d
Márta Boldizsár <marta.boldi13@gmail.com>
2019-03-04 12:20:01 +0100

    added login redirection

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = new express();
 
 app.use(express.static('static'));
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/static/login.html");
+});
+
 var port = 3000;
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
