77705d086e653d77a08cbd02062423d410af7192
Márta Boldizsár <marta.boldi13@gmail.com>
2019-05-04 16:45:31 +0200

    forgot password recovery

--- middleware/generic/checkUserLogin.js
+++ middleware/generic/checkUserLogin.js
@@ -10,6 +10,8 @@ module.exports = function (objectrepository) {
     const userModel = requireOption(objectrepository, 'userModel');
 
     return function (req, res, next) {
+        res.local = {};
+
         if ((typeof req.query.email === 'undefined') ||
             (typeof req.query.password === 'undefined')) {
             return next();
@@ -20,16 +22,23 @@ module.exports = function (objectrepository) {
             function (err, result) {
                 if(err || !result) {
                     console.log("email not in database");
-                    return res.redirect('/login');
+                    if(req.query.email !== "")
+                        res.local.error = "Nincs ilyen email cím az adatbázisban, regisztráld be!";
+                    else
+                        res.local.error = "Nem adtál meg email címet!";
+                    return next();
                 }
-
                 if(result.password === req.query.password) {
                     req.session.userid = result.id;
                     return res.redirect('/rent/list');
                 }
                 else {
                     console.log("Wrong password :(");
-                    return res.redirect('/login')
+                    if(req.query.password === "")
+                        res.local.error = "Nem adtál meg jelszót!";
+                    else
+                        res.local.error = "Hibás jelszó!";
+                    return next();
                 }
             }
         );
--- middleware/user/sendUserPassw.js
+++ middleware/user/sendUserPassw.js
@@ -3,9 +3,33 @@
  * description
  */
 
+const requireOption = require('../common').requireOption;
+
 module.exports = function (objectrepository) {
 
+    const userModel = requireOption(objectrepository, 'userModel');
+
     return function (req, res, next) {
-        return next();
+        res.local = {};
+
+        if(typeof req.query.email !== 'undefined') {
+            userModel.findOne(
+                {email: req.query.email},
+                function (err, result) {
+                    if(!result) {
+                        if(req.query.email !== "")
+                            res.local.error = "Nincs ilyen email cím az adatbázisban, regisztráld be!";
+                    }
+                    else {
+                        res.local.pw = result.password;
+                    }
+                    return next();
+                }
+            )
+        }
+        else {
+            return next();
+        }
+
     };
 };
